d785a8676f56860e51a4eb89208f4ebf6544cd7f
GayathriChakravarthy <51126893+GayathriChakravarthy@users.noreply.github.com>
2021-04-12 19:10:05 +0100

    Update lamda_function.py
    Add comments

--- lambda/lamda_function.py
+++ lambda/lamda_function.py
@@ -1,9 +1,9 @@
-#import json
 import boto3
 import pandas
 import time
 import timeit
 
+# initialise
 s3_client = boto3.client('s3')
 glue_client = boto3.client('glue')
 
@@ -13,22 +13,29 @@ def lambda_handler(event, context):
     
     df = get_s3_dataset_df(bucket_name, key)
     
+    # quality check 01 - check df has data
     if (check_df_empty(df)):
         print(f'Dataset returned empty - please check file, {bucket_name}/{key}')
     else:
         print('Check passed - dataset exists')
         
+    # quality check 02 - check null data for key fields. Pressure field added to show the check works
     null_check_fields = ['ObservationDate', 'Region', 'ScreenTemperature', 'Pressure']
     
     for col in null_check_fields:
         check_null(df, col)
+    
+    # more data quality checks can go here...
         
+    # this function triggers all the Glue processing so the csv file is converted to Parquet
+    # and is Athena query ready
     trigger_glue_tasks()
     
     return 1
     
 def get_s3_dataset_df(bucket_name, key):
     
+    # lambda can download to /tmp/ only
     local_file = '/tmp/' + 'weather_data.csv'
     df = pandas.DataFrame()
     
@@ -41,8 +48,6 @@ def get_s3_dataset_df(bucket_name, key):
     except Exception as e:
         print(e)
         raise e
-    # resp = s3_client.get_object(Bucket=bucket_name, Key=key)
-    # data = resp['Body'].read().decode('utf-8')
     
 def check_df_empty(df):
     if df.empty:
@@ -58,10 +63,12 @@ def trigger_glue_tasks():
     start_time = timeit.default_timer()
     abort_time = start_time + timeout_seconds
     
+    # helps handle timeout state
     def check_for_timeout() -> None:
         if timeit.default_timer() > abort_time:
             raise TimeoutError(f"Failed to crawl {crawler}. The allocated time of {timeout_minutes:,} minutes has elapsed.")
     
+    # function to trigger Glue crawlers
     def run_and_check_crawler(crawler):
         state_previous = None
         retry_seconds = 10
@@ -79,6 +86,7 @@ def trigger_glue_tasks():
             check_for_timeout()
             time.sleep(retry_seconds)
     
+    # function to trigger Glue job for Parquet conversion
     def run_and_check_job(job):
         state_previous = None
         retry_seconds = 10
